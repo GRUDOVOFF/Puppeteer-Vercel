de03c87538ec0897fff63ccebd118c5c4197dd8e
GRUDOVOFF <52879102+GRUDOVOFF@users.noreply.github.com>
2022-12-11 19:05:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const app = require("express")();
-
+import captureWebsite from 'capture-website';
 let chrome = {};
 let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
+    chrome = require("chrome-aws-lambda");
+    puppeteer = require("puppeteer-core");
 } else {
-  puppeteer = require("puppeteer");
+    puppeteer = require("puppeteer");
 }
 
 app.get("/api", async (req, res) => {
-  let options = {};
+    let options = {};
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+            args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        };
+    }
 
-  try {
-    let browser = await puppeteer.launch(options);
-
-    let page = await browser.newPage();
-    await page.goto("https://grudov.ru");
-    res.send(await page.title());
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
+    try {
+        let browser = await puppeteer.launch(options);
+        await captureWebsite.file('https://grudov.ru', 'screenshot.png');
+        let page = await browser.newPage();
+        await page.goto("https://grudov.ru");
+        res.send(await page.title());
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+    console.log("Server started");
 });
 
 module.exports = app;
